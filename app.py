3dacb435c37c2d5e0725012fa10dff91f528add7
GamePatchHub88 <mh3976698@gmail.com>
2025-05-01 22:10:28 +0300

    app.py

--- app.py
+++ app.py
@@ -26,5 +26,5 @@ def upload():
 
     return send_file(output_path, as_attachment=True)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
